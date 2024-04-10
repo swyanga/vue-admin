8dc583b8230e57fe536d5177e8150eae79a048b9
杨思伟 <yangsiwei@MacBook-Pro.local>
2024-04-10 22:59:15 +0800

    转换env环境变量类型

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,18 @@
-import { defineConfig } from 'vite'
+import { ConfigEnv, defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import alias from './vite/alias';
+import { parseEnv } from './vite/util';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  // 配置别名
-  resolve: {
-    alias
+export default ({ command, mode }: ConfigEnv) => {
+  const isBuild = command === 'build';
+  const root = process.cwd()
+  const env = parseEnv(loadEnv(mode, root));
+
+  return {
+    plugins: [vue()],
+    // 配置别名
+    resolve: {
+      alias
+    }
   }
-})
+}
--- vite/util.ts
+++ vite/util.ts
@@ -1,8 +1,7 @@
 import _ from 'lodash'
 
-
+// 解析 env 环境变量
 export function parseEnv(env: Record<string, any>) {
-	console.log('env', env)
 	const envs = _.cloneDeep(env)
 	Object.entries(env).forEach(([key, value]) => {
 		if (value == 'true' || value == 'false') {

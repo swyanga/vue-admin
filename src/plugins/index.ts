8da44678ccce31dc44e692304d7814f514da6fca
杨思伟 <yangsiwei@MacBook-Pro.local>
2024-04-21 21:31:02 +0800

    完成全局组件注册

--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -1,8 +1,20 @@
 import { App } from 'vue'
 import { setupTailwindcss } from './tailwindcss'
+import _ from 'lodash'
 
 export function setupPlugins(app: App) {
+  autoRegisterComponent(app)
   // 注册插件
   setupTailwindcss()
-  // ...
+}
+
+// 自动注册全局组件
+function autoRegisterComponent(app: App) {
+  // 读取所有组件
+  const components = import.meta.glob('../components/**/*.vue', { eager: true, import: 'default' })
+  // 遍历组件
+  Object.keys(components).forEach(key => {
+    const name = key.split('/').pop()?.split('.').shift() as string;
+    app.component(_.camelCase(name), components[key] || {})
+  })
 }
--- src/views/auth/login.vue
+++ src/views/auth/login.vue
@@ -1,7 +1,4 @@
 <script lang="ts" setup>
-import hdInput from '@/components/form/hd-input.vue';
-import hdButton from '@/components/form/hd-button.vue';
-import hdLink from '@/components/form/hd-link.vue';
 </script>
 
 <template>
@@ -11,16 +8,11 @@ import hdLink from '@/components/form/hd-link.vue';
       <div class="p-6">
         <h2 class="text-center text-gray-700 text-lg mt-3">会员登录</h2>
         <div class="mt-8">
-          <!-- <input type="text" placeholder="请输入手机号或邮箱" class="hd-input">
-          <input type="text" placeholder="请输入手机号或邮箱" class="hd-input mt-3"> -->
           <hdInput />
           <hdInput class="mt-5" />
-          <hdButton />
         </div>
-
+        <hdButton />
         <div class="flex gap-2 justify-center mt-5">
-          <!-- <a href="" class="text-xs text-gray-700">网站首页</a> -->
-          <hdLink />
           <a href="" class="text-xs text-gray-700">会员注册</a>
           <a href="" class="text-xs text-gray-700">找回密码</a>
           <a href="" class="text-xs text-gray-700">找回密码</a>
